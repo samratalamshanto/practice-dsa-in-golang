56b5b85ae51d72be5aa45b0520ec79daf8031aff
Samrat Alam <samratalamshanto710@gmail.com>
2025-09-30 21:18:29 +0600

    Refactor: stack in oop style

--- ds/stack/stack.go
+++ ds/stack/stack.go
@@ -2,17 +2,42 @@ package main
 
 import "fmt"
 
-func main() {
-	stack := []int{}
-	stack = append(stack, 1)
-	stack = append(stack, 2, 3, 4, 5) //add
+type Stack struct {
+	data []int
+}
 
+func (s *Stack) Append(val int) {
+	s.data = append(s.data, val)
+}
+
+func (s *Stack) Pop() {
+	if len(s.data) == 0 {
+		return
+	}
+	s.data = s.data[:len(s.data)-1]
+}
+
+func (s *Stack) Top() int {
+	if len(s.data) == 0 {
+		return -1
+	}
+	return s.data[len(s.data)-1]
+}
+
+func main() {
+	stack := &Stack{}
 	fmt.Println(stack)
 
-	top := stack[len(stack)-1] //top
+	stack.Append(2)
+	stack.Append(3)
+	stack.Append(4)
+	stack.Append(5)
+	fmt.Println(stack)
+
+	top := stack.Top()
 	fmt.Println(top)
 
-	stack = stack[:len(stack)-1] //pop
-	fmt.Println(stack)
+	stack.Pop()
+	fmt.Println(stack.data)
 
 }
